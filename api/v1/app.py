43dba3af98b1978610142a57ba801aea65879628
martinliporace <martin.liporace@gmail.com>
2022-09-08 23:56:37 +0000

    update with handler error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """ starts a Flask web application"""
 
 from os import getenv
-from flask import Flask, Blueprint, abort
+from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -14,6 +14,13 @@ app.register_blueprint(app_views)
 def close(self):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """ note that we set the 404 status explicitly """
+    message = {'error': 'Not found'}
+    return (jsonify(message), 404)
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST")
