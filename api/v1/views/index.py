d763eb91e91112475d1ae8db6de441712bd65d2d
martinliporace <martin.liporace@gmail.com>
2022-09-08 23:26:28 +0000

    Fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,9 +16,9 @@ def status():
 def stats():
     """ Returns the number of each objects by type """
     dict = {"amenities": storage.count('Amenity'),
-            "cities": storage.count('City'), 
-            "places": storage.count('Place'), 
-            "reviews": storage.count('Review'), 
-            "states": storage.count('State'), 
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
             "users": storage.count('User')}
     return jsonify(dict)
