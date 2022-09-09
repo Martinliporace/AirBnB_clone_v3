621e5666dbdec54d8b392de7c1a6dc4c4b67cc46
martinliporace <martin.liporace@gmail.com>
2022-09-09 14:01:25 +0000

    updated with delete, put and post methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from models.state import State
 
 
-@app_views.route("/states", methods=['GET'])
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
     all = []
@@ -16,7 +16,7 @@ def get_states():
     return jsonify(all)
 
 
-@app_views.route("/states/<state_id>", methods=['GET'])
+@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
 def get_state(state_id=None):
     """Retrieves a State object"""
     if not state_id:
@@ -26,3 +26,46 @@ def get_state(state_id=None):
         if st is None:
             abort(404)
         return jsonify(st.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete(state_id=None):
+    """Deletes a State object"""
+    st = storage.get(State, state_id)
+    if st is None:
+        abort(404)
+    else:
+        storage.delete(st)
+        storage.save()
+        return (jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create():
+    """ Creates a state """
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
+    elif "name" not in req:
+        abort(400, "Missing name")
+    new = State(name=req['name'])
+    storage.new(new)
+    storage.save()
+    return (jsonify(new.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update(state_id=None):
+    """ update state """
+    st = storage.get(State, state_id)
+    attr = ['name', 'id', 'created_at', 'updated_at']
+    if st is None:
+        abort(404)
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
+    for key, value in st.items():
+        if key not in attr:
+            setattr(st, key, value)
+    st.save()
+    return (jsonify(st.to_dict()), 201)
