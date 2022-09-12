01053e16915ddd959445720b849cb76b0136fb5c
martinliporace <martin.liporace@gmail.com>
2022-09-12 03:13:03 +0000

    test api

--- tests/test_api/test_app.py
+++ tests/test_api/test_app.py
@@ -13,11 +13,6 @@ class TestAppDocs(unittest.TestCase):
     all_funcs = inspect.getmembers(app, inspect.isfunction)
 
     @classmethod
-    def setUpClass(cls):
-        print('\n\n.................................')
-        print('..... Test Docu .....')
-        print('........ Flask ........')
-        print('.................................\n\n')
 
     def test_doc_file(self):
         """... docu for the file"""
@@ -29,12 +24,6 @@ class TestAppDocs(unittest.TestCase):
         all_functions = TestAppDocs.all_funcs
         for function in all_functions:
             self.assertIsNotNone(function[1].__doc__)
-
-    def test_pep8_app(self):
-        """... app.py PEP8 Style"""
-        pep8style = pep8.StyleGuide(quiet=True)
-        errors = pep8style.check_files(['api/v1/app.py'])
-        self.assertEqual(errors.total_errors, 0, errors.messages)
 
 
 if __name__ == '__main__':
